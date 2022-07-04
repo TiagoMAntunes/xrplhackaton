ff7f196dca34e686ef85f93b6caf1f6bfdc7395a
TiagoMAntunes <tiago.melo.antunes@gmail.com>
2022-07-04 11:55:52 +0100

    Added payment popup

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,24 +1,53 @@
 import React from "react";
-import { Button, Card, CardBody, CardImg, CardTitle } from "shards-react";
+import { Button, Card, CardBody, CardImg, CardTitle, FormInput, Modal, ModalBody, ModalFooter, ModalHeader } from "shards-react";
 
 export default class Product extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { open: false, amount: null };
+        this.payment = this.payment.bind(this);
+        this.inputMoney = this.inputMoney.bind(this);
+    }
+
     format(price) {
-        return "$" + price.toFixed(2);
+        return "XRP " + price.toFixed(2);
+    }
+
+    payment() {
+        this.setState({ open: !this.state.open });
+    }
+
+    inputMoney(m) {
+        this.setState({ amount: m.target.value });
     }
 
     render() {
         return (
-            <Card style={{ maxWidth: "300px", margin: "20px" }}>
-                <CardImg src={this.props.image} />
-                <CardBody style={{ height: "200px", textAlign: "left" }}>
-                    <CardTitle>{this.props.name}</CardTitle>
-                    <p>{this.props.description}</p>
-                </CardBody>
-                <CardBody>
-                    <p>Already raised:  {this.format(this.props.price)}</p>
-                    <Button theme="primary">Donate</Button>
-                </CardBody>
-            </Card>
+            <div>
+                <Card style={{ maxWidth: "300px", margin: "20px" }}>
+                    <CardImg src={this.props.image} />
+                    <CardBody style={{ height: "200px", textAlign: "left" }}>
+                        <CardTitle>{this.props.name}</CardTitle>
+                        <p>{this.props.description}</p>
+                    </CardBody>
+                    <CardBody>
+                        <p>Already raised:  {this.format(this.props.price)}</p>
+                        <Button theme="primary" onClick={this.payment}>Donate</Button>
+                    </CardBody>
+                </Card>
+
+                <Modal open={this.state.open} toggle={this.payment}>
+                    <ModalHeader>Payment</ModalHeader>
+                    <ModalBody>
+                        <p>Thank you for supporting {this.props.name}! Please input below the amount you wish to donate:</p>
+                        <FormInput type="number" placeholder="Amount" onChange={this.inputMoney} />
+                    </ModalBody>
+
+                    <ModalFooter>
+                        <Button>Pay</Button>
+                    </ModalFooter>
+                </Modal>
+            </div>
         );
     }
 }
